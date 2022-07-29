2a492c5dcdaf35a53f71a4840c0b805ee4185ee0
Francois Smith <steamfaffa@gmail.com>
2022-07-29 15:54:56 +0200

    Removed Debug Folder

--- Practicals/Practical 1/cmake-build-debug/cmake_install.cmake
+++ Practicals/Practical 1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/steam/Desktop/University/COS-214/Practicals/Practical 1
+# Install script for directory: C:/Users/steam/Desktop/University/COS214/Practicals/Practical 1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/steam/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5921.27/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/steam/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/222.3345.126/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/steam/Desktop/University/COS-214/Practicals/Practical 1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/steam/Desktop/University/COS214/Practicals/Practical 1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
