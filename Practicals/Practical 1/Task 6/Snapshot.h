d8edf15673d7cbbb3475a22d72ca22b852f6e568
Francois Smith <steamfaffa@gmail.com>
2022-07-31 20:55:01 +0200

    Changed to Friend Class

--- Practicals/Practical 1/Task 6/Snapshot.h
+++ Practicals/Practical 1/Task 6/Snapshot.h
@@ -9,10 +9,9 @@ class Snapshot: public AuditableSnapshot
 private:
     std::string state_;
     std::string date_;
-
-public:
     explicit Snapshot(const std::string& state);
     std::string GetUsername() const override;
     std::string date() const override;
     std::string state() const override;
+    friend class User;
 };
