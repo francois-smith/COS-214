68ae2cf4e6c2a8adeb4cb83531bfba6cedfe9af1
Francois Smith <steamfaffa@gmail.com>
2022-08-04 22:14:02 +0200

    Added Docs To Base Factory

--- Practicals/Practical 2/EnemyFactory.cpp
+++ Practicals/Practical 2/EnemyFactory.cpp
@@ -1,6 +1,18 @@
+/*
+ * EnemyFactory.cpp
+ *
+ *  Created on: 04 Aug 2022
+ *      Author: Francois Smith
+ */
+
 #include "EnemyFactory.h"
 
-std::string EnemyFactory::getName() {
+/**
+ * Generates a random name for an enemy.
+ * @return Generated Name
+ */
+std::string EnemyFactory::getName()
+{
     std::string names[] = {"Snake", "Gorilla", "Jaguars", "Cannibal"};
     srand(time(nullptr));
     return names[rand() % 4];
--- Practicals/Practical 2/EnemyFactory.h
+++ Practicals/Practical 2/EnemyFactory.h
@@ -1,16 +1,39 @@
+/*
+ * EnemyFactory.h
+ *
+ *  Created on: 04 Aug 2022
+ *      Author: Francois Smith
+ */
+
 #pragma once
 #include "Enemy.h"
 #include <ctime>
 #include <string>
 #include <random>
 
-class EnemyFactory {
+/**
+ * Abstract factory class used to create enemies. \n
+ * Contains a method that returns created instances of enemies, as well as a name generator for enemies.
+ */
+class EnemyFactory
+{
 public:
-    EnemyFactory() {}
-    virtual ~EnemyFactory() {}
+    //Constructor and Destructor
+    EnemyFactory() = default;
+    virtual ~EnemyFactory() = default;
+
+    //Factory Method
+    /**
+     * Creates and returns a pointer to a new Enemy. \n
+     * Returns base class type to allow numerous types of enemies to be stored in a single array.
+     * @param attack - Attack Value Of Enemy
+     * @param defense - Defence Value Of Enemy
+     * @return Pointer to newly created enemy
+     */
     virtual Enemy* createEnemy(std::string attack, std::string defense) = 0;
 
 protected:
+    //Helper Functions
     std::string getName();
 };
 
