ee734edadcf0996fe015e403b4daa890f640934d
Francois Smith <steamfaffa@gmail.com>
2022-08-04 22:13:44 +0200

    Expanded Enemy Base Class

--- Practicals/Practical 2/Enemy.cpp
+++ Practicals/Practical 2/Enemy.cpp
@@ -1,11 +1,24 @@
+/*
+ * Enemy.cpp
+ *
+ *  Created on: 02 Aug 2022
+ *      Author: Francois Smith
+ */
+
 #include "Enemy.h"
 
-Enemy::Enemy(int hp, int dmg, std::string& atk, std::string& name, std::string& def) {
+Enemy::Enemy(int hp, int dmg, std::string& atk, std::string& name, std::string& def)
+{
     this->hp = hp;
     this->dmg = dmg;
     this->atk = atk;
     this->def = def;
     this->name = name;
+}
+
+Enemy::~Enemy()
+{
+
 }
 
 /**
@@ -15,20 +28,20 @@ Enemy::Enemy(int hp, int dmg, std::string& atk, std::string& name, std::string& 
  */
 void Enemy::attack(SquadMember *z)
 {
-    if(z->getHP() > 0)
+    while(z->getHP() > 0 and this->getHP() > 0)
     {
-       if(hitSquadMember(z))
-       {
-           z->die();
-           this->celebrate();
-       }
-       else
-       {
-           if(getHit(z))
-           {
-               this->die();
-           }
-       }
+        if(hitSquadMember(z))
+        {
+            z->die();
+            this->celebrate();
+        }
+        else
+        {
+            if(getHit(z))
+            {
+                this->die();
+            }
+        }
     }
 }
 
@@ -60,6 +73,15 @@ int Enemy::getDmg() const
 }
 
 /**
+ * Sets the new Damage value of the enemy.
+ * @param newDef - New Damage Value
+ */
+void Enemy::setDamage(int newDmg)
+{
+    this->dmg = newDmg;
+}
+
+/**
  * Returns the attack move of the enemy.
  * @return  Attack Move
  */
@@ -69,10 +91,55 @@ std::string Enemy::getAtk() const
 }
 
 /**
+ * Sets the new Attack move of the enemy.
+ * @param newAtk - New Attack Property
+ */
+void Enemy::setAtk(std::string newAtk)
+
+    this->atk = newAtk;
+}
+
+/**
  * Returns the defense move of the enemy.
  * @return Defense Move
  */
 std::string Enemy::getDef() const
 {
     return this->def;
+}
+
+/**
+ * Sets the new Defence move of the enemy.
+ * @param newDef - New Defence Property
+ */
+void Enemy::setDef(std::string newDef)
+{
+    this->def = newDef;
+}
+
+/**
+ * Returns the name of the enemy.
+ * @return Name of Enemy
+ */
+std::string Enemy::getName() const
+{
+    return this->name;
+}
+
+/**
+ * Sets the new Name move of the enemy.
+ * @param newName - New Name
+ */
+void Enemy::setName(std::string newName)
+{
+    this->name = newName;
+}
+
+/**
+ * Prints the enemy's name, Damage and HP. Used for debugging.
+ * @return
+ */
+std::string Enemy::print()
+{
+    return "Enemy: " + this->getName()+"\nHP: "+std::to_string(getHP())+"\nDamage: "+std::to_string(getDmg())+"\n";
 }
--- Practicals/Practical 2/Enemy.h
+++ Practicals/Practical 2/Enemy.h
@@ -1,29 +1,77 @@
+/*
+ * Enemy.h
+ *
+ *  Created on: 02 Aug 2022
+ *      Author: Francois Smith
+ */
+
 #pragma once
 #include "SquadMember.h"
-#include <string>
 #include <iostream>
 
 /**
- * Abstract class for all enemy types.
+ * Abstract class for all enemy types. \n
  * Defines a base attack function that follows the same logic for all enemies.
- *
  */
-class Enemy {
+class Enemy
+{
 public:
+    //Constructor and Destructor
     Enemy(int hp, int dmg, std::string& atk, std::string& name, std::string& def);
+    virtual ~Enemy();
+
+    //Template Method
     void attack(SquadMember* z);
-    void setHP(int hp);
+
+    //Setters and Getters
     int getHP() const;
+    void setHP(int newHp);
+
     int getDmg() const;
+    void setDamage(int newDmg);
+
     std::string getName() const;
+    void setName(std::string newName);
+
     std::string getAtk() const;
+    void setAtk(std::string newAtk);
+
     std::string getDef() const;
-    virtual bool hitSquadMember(SquadMember* z) = 0;
-    virtual bool getHit(SquadMember* z) = 0;
-    virtual void celebrate() = 0;
-    virtual void die() = 0;
+    void setDef(std::string newDef);
+
+    //Helper Function
+    std::string print();
 
 protected:
+    //Template Functions
+    /**
+     * The enemy hits the squad member by calling the SquadMember's takeDamage method,
+     * this method takes the damage done by the enemy as a parameter.
+     * @param z - SquadMember to Attack
+     * @return true if the squad member is killed, false otherwise
+     */
+    virtual bool hitSquadMember(SquadMember* z) = 0;
+
+    /**
+     * Called when SquadMember is still alive after being hit by an enemy. \n
+     * Damages the current enemy by retrieving the squad members damage value. The value is then subtracted from the current enemy's HP.
+     * @param z - SquadMember Attacking
+     * @return if the enemy is killed
+     */
+    virtual bool getHit(SquadMember* z) = 0;
+
+    /**
+     * Generic function that is called when the enemy is killed. Outputs a celebratory message.
+     */
+    virtual void celebrate() = 0;
+
+    /**
+     * If SquadMember kills an enemy a death message is outputted
+     */
+    virtual void die() = 0;
+
+private:
+    //Member Variables
     int hp;
     int dmg;
     std::string atk;
