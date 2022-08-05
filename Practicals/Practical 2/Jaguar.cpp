d08b00c59380250ad0c7ee4dd1c311d9b2df26e9
Francois Smith <steamfaffa@gmail.com>
2022-08-05 21:23:44 +0200

    Fixed Jaguar Syntax

--- Practicals/Practical 2/Jaguar.cpp
+++ Practicals/Practical 2/Jaguar.cpp
@@ -13,7 +13,7 @@ bool Jaguar::hitSquadMember(SquadMember* z)
 {
     if(z == nullptr) return false;
 
-    std::cout << "Jaguar leaps toward the  " << z->getName() << " and delivers a forceful " << this->getAtk() << "." << std::endl;
+    std::cout << "Jaguar leaps toward " << z->getName() << " and delivers a forceful " << this->getAtk() << "." << std::endl;
     if(z->takeDamage(this->getDmg()) <= 0)
     {
         return true;
@@ -32,7 +32,7 @@ bool Jaguar::getHit(SquadMember* z)
 {
     if(z == nullptr) return false;
 
-    std::cout << "Growls in pain as he is maimed. Jaguar turns around and delivers " << this->getDef() << " against" << z->getName() << "." << std::endl;
+    std::cout << "Jaguar growls in pain as he is maimed. Jaguar turns around and delivers " << this->getDef() << " against " << z->getName() << "." << std::endl;
     this->setHP(this->getHP() - z->getDmg());
     if(this->getHP() <= 0)
     {
@@ -45,5 +45,5 @@ bool Jaguar::getHit(SquadMember* z)
 
 void Jaguar::die()
 {
-    std::cout << "Gives one last growl before falling over" << std::endl;
+    std::cout << "Jaguar gives one last growl before falling over." << std::endl;
 }
