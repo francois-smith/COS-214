f2e29dddb5127b5094a603805177d0b8427477f9
Francois Smith <steamfaffa@gmail.com>
2022-08-05 21:21:27 +0200

    Expanded SquadMembers

--- Practicals/Practical 2/SquadMember.cpp
+++ Practicals/Practical 2/SquadMember.cpp
@@ -1,52 +1,133 @@
+/*
+ * SquadMember.cpp
+ *
+ *  Created on: 05 Aug 2022
+ *      Author: Francois Smith
+ */
+
 #include "SquadMember.h"
 
-SquadMember::SquadMember(int hp, std::string& name, int dmg)
+SquadMember::SquadMember(int hp, int dmg)
 {
     this->hp = hp;
-    this->name = name;
     this->dmg = dmg;
 }
 
+SquadMember::SquadMember(int hp, int dmg, std::string& name) {
+    this->hp = hp;
+    this->dmg = dmg;
+    this->name = name;
+}
+
+/**
+ * Returns the current Damage value of the enemy.
+ * @return Current Damage value.
+*/
+int SquadMember::getDmg() const
+{
+    return this->dmg;
+}
+
+/**
+ * Sets the new Damage value of the SquadMember.
+ * @param newDmg - New Damage Value
+ */
+void SquadMember::setDmg(int newDmg)
+{
+    this->dmg = newDmg;
+}
+
+/**
+ * Sets the new HP value of the SquadMember.
+ * @param newHp - New HP Value
+ */
 void SquadMember::setHP(int newHp)
 {
     this->hp = newHp;
 }
 
+/**
+ * Sets the new name of the SquadMember.
+ * @param newName - New Name
+ */
+void SquadMember::setName(std::string& newName)
+{
+    this->name = newName;
+}
+
+/**
+ * Returns the current name of the SquadMember.
+ * @return Current name
+*/
 std::string SquadMember::getName() const
 {
     return this->name;
 }
 
+/**
+ * Returns the current HP value of the SquadMember.
+ * @return Current HP value.
+*/
+int SquadMember::getHP() const
+{
+    return this->hp;
+}
+
+/**
+ * Returns the type of SquadMember.
+ * @return Type.
+*/
+std::string SquadMember::getType() const
+{
+    return this->type;
+}
+
+/**
+ * Sets type of SquadMember.
+ * @param newType - New Type
+*/
+void SquadMember::setType(std::string &newType)
+{
+    this->type = newType;
+}
+
+/**
+ * Outputs death message when a SquadMember is killed. \n
+ * Clears memory used by SquadMember object.
+ */
+void SquadMember::die() const
+{
+    std::cout << "SquadMember " << this->getName() << " has died." << std::endl;
+}
+
+/**
+ * Damages the current SquadMember by subtracting the damage value of the enemy from the current HP value. \n
+ * @param damage - Damage Value of Enemy
+ * @return If the SquadMember is killed
+ */
 int SquadMember::takeDamage(int damage)
 {
     setHP(this->hp - damage);
     return this->hp;
 }
 
-int SquadMember::getHP() const
+/**
+ * Generates a random name for a SquadMember.
+ * @return Generated Name
+ */
+std::string SquadMember::generateName() {
+    return "SquadMember";
+}
+
+
+
+/**
+ * Prints the SquadMember's name, Damage and HP. Used for debugging.
+ * @return Debug Message
+ */
+void SquadMember::printStats() const
 {
-    return this->hp;
-}
-
-void SquadMember::die()
-{
-
-}
-
-int SquadMember::getDmg() const
-{
-    return this->dmg;
-}
-
-void SquadMember::setDmg(int newDmg)
-{
-    this->dmg = newDmg;
-}
-
-int SquadMember::getDamage() const {
-    return this->dmg;
-}
-
-void SquadMember::setDamage(int newDmg) {
-    this->dmg = newDmg;
+    std::cout << "------------------------------------------" << std::endl;
+    std::cout << "Type: " << this->getType() << " | HP: " << this->getHP() << " | DMG: " << this->getDmg() << std::endl;
+    std::cout << "------------------------------------------" << std::endl << std::endl;
 }
--- Practicals/Practical 2/SquadMember.h
+++ Practicals/Practical 2/SquadMember.h
@@ -1,28 +1,59 @@
-#pragma once
-#include <string>
+/*
+ * SquadMember.h
+ *
+ *  Created on: 05 Aug 2022
+ *      Author: Francois Smith
+ */
 
+#pragma once
+#include <iostream>
+#include <string>
+#include <random>
+
+/**
+ * Abstract class for all SquadMember types. \n
+ * Contains base functionality needed by all SquadMembers, contains abstract methods for cloning to implement the prototype pattern.
+ */
 class SquadMember
 {
 public:
-    SquadMember(int hp, std::string& name, int dmg);
+    //s and Destructor
+    SquadMember() = default;
+    SquadMember(int hp, int dmg);
+    SquadMember(int hp, int dmg, std::string& name);
+    virtual ~SquadMember() = default;
+
+    //Getters and Setters
     int getDmg() const;
     void setDmg(int newDmg);
+
     int getHP() const;
     void setHP(int newHp);
-    int getDamage() const;
-    void setDamage(int newDmg);
+
+    std::string getType() const;
+    void setType(std::string& newType);
+
     std::string getName() const;
+    void setName(std::string& newName);
+
+    //Member Functions
     int takeDamage(int damage);
-    void die();
-    virtual SquadMember* clone() = 0;
+    void die() const;
 
+    //Template and Prototype Functions
     /**
-     * Generates a random name for an SquadMember.
-     * @return Generated Name
+     * Virtual clone method implemented within sub SquadMember classes.
+     * @return - Pointer to a new SquadMember object.
      */
-    virtual std::string generateName() = 0;
+    virtual SquadMember* clone() = 0;
+    virtual std::string generateName();
 
-protected:
+    //Helper Function
+    void printStats() const;
+
+private:
+    //Member Variables
+    std::string type;
     std::string name;
     int dmg;
     int hp;
